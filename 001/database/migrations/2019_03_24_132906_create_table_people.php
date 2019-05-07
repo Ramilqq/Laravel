995b9a89c2d0dbffc3c2e99bae4b06ffbd98bd1f
Ramil <47780103+Ramilqq@users.noreply.github.com>
2019-05-07 15:01:07 +0300

    Add files via upload

---
+++ 001/database/migrations/2019_03_24_132906_create_table_people.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTablePeople extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('people', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name',200);
+            $table->string('position',200);
+            $table->text('text');
+            $table->string('images',100);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('people');
+    }
+}
